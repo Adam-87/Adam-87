2e25b092822bdf4f70d565043b1d566ca1df5988
Adam-87 <abance155@gmail.com>
2025-04-09 18:36:07 +0100

    Create Pin_Definitions.h

---
+++ Embedded_Systems/Pin_Definitions.h
@@ -0,0 +1,18 @@
+#define LCD_B P2_6
+#define Master_C P1_7
+#define Data_IN P2_5
+#define S1 P2_3
+#define A P2_1
+#define B P1_5
+#define E2 P1_3
+
+
+
+
+#define Data_OUT P2_7
+#define Sel_OR P1_6
+#define S0 P1_4
+#define S2 P2_2
+#define B P2_0
+#define E3 P1_4 
+#define E1 P1_0
